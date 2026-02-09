5f24c186a515a1f515d03d47b30c4d6b9f110fca
Veysel Murat OKSAK <vmuratoksak2003@icloud.com>
2026-02-09 20:26:38 +0300

    j

--- WebApplication1/Models/HelloViewModel.cs
+++ WebApplication1/Models/HelloViewModel.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
     public class HelloViewModel
     {
         [Required(ErrorMessage = "İsim zorunludur")]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
-        public string? Message { get; set; }
-
+        public string Message { get; set; }
         public DateTime Date { get; set; }
     }
 }
