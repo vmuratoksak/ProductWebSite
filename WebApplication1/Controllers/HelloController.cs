e8a0da71fc9cc1473b368192ace8977d30186d6b
Veysel Murat OKSAK <vmuratoksak2003@icloud.com>
2026-02-09 20:19:19 +0300

    su an form duzgun calisiyor

--- WebApplication1/Controllers/HelloController.cs
+++ WebApplication1/Controllers/HelloController.cs
@@ -1,35 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
-using System;
 
 namespace WebApplication1.Controllers
 {
     public class HelloController : Controller
     {
-        // SAYFA İLK AÇILDIĞINDA
         [HttpGet]
         public IActionResult Index()
         {
-            var model = new HelloViewModel
+            return View(new HelloViewModel
             {
-                Message = "Lütfen adınızı girin",
                 Date = DateTime.Now
-            };
-
-            return View(model);
+            });
         }
 
-        // FORM GÖNDERİLDİĞİNDE
         [HttpPost]
         public IActionResult Index(HelloViewModel model)
         {
-            // GEÇİCİ OLARAK ModelState KONTROLÜNÜ KALDIR
-            model.Message = $"Merhaba {model.Name} 👋";
             model.Date = DateTime.Now;
 
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            model.Message = $"Merhaba {model.Name} 👋";
             return View(model);
         }
-
     }
 }
-
--- WebApplication1/Models/HelloViewModel.cs
+++ WebApplication1/Models/HelloViewModel.cs
@@ -5,11 +5,11 @@ namespace WebApplication1.Models
 {
     public class HelloViewModel
     {
-        
         [Required(ErrorMessage = "İsim zorunludur")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string Message { get; set; }
+        public string? Message { get; set; }
+
         public DateTime Date { get; set; }
     }
 }
