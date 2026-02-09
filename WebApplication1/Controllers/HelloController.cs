b37fa2974e030ecf3dadff071473ed164e714fb6
Veysel Murat OKSAK <vmuratoksak2003@icloud.com>
2026-02-09 20:23:01 +0300

    TempData

--- WebApplication1/Controllers/HelloController.cs
+++ WebApplication1/Controllers/HelloController.cs
@@ -1,31 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using System;
 
 namespace WebApplication1.Controllers
 {
     public class HelloController : Controller
     {
+        // GET → Formu göster
         [HttpGet]
         public IActionResult Index()
         {
-            return View(new HelloViewModel
+            var model = new HelloViewModel
             {
+                Message = "Lütfen adınızı girin",
                 Date = DateTime.Now
-            });
+            };
+
+            // TempData varsa oku
+            if (TempData["Message"] != null)
+            {
+                model.Message = TempData["Message"].ToString();
+            }
+
+            return View(model);
         }
 
+        // POST → İşle → Redirect
         [HttpPost]
         public IActionResult Index(HelloViewModel model)
         {
-            model.Date = DateTime.Now;
-
             if (!ModelState.IsValid)
             {
-                return View(model);
+                TempData["Message"] = "Lütfen adınızı girin";
+                return RedirectToAction("Index");
             }
 
-            model.Message = $"Merhaba {model.Name} 👋";
-            return View(model);
+            TempData["Message"] = $"Merhaba {model.Name} 👋";
+            return RedirectToAction("Index");
         }
     }
 }
