760169397e0382a06461bd20e0ba58dddc367286
Veysel Murat OKSAK <vmuratoksak2003@icloud.com>
2026-02-11 16:42:46 +0300

    dsds

--- WebApplication1/Models/HelloIndexViewModel.cs
+++ WebApplication1/Models/HelloIndexViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
+using WebApplication1.Models;
 
 namespace WebApplication1.Models
 {
     public class HelloIndexViewModel
     {
-        public List<NameEntity> Names { get; set; } = new();
-
+        public string NewName { get; set; }
         public string SearchTerm { get; set; }
+        public List<NameEntity> Names { get; set; }
 
         public int CurrentPage { get; set; }
 
