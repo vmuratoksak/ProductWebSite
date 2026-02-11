6f073667b1ada2c8c0d00906cc67731d09a1ad03
Veysel Murat OKSAK <vmuratoksak2003@icloud.com>
2026-02-11 17:29:39 +0300

    update date added

--- WebApplication1/Models/HelloEntity.cs
+++ WebApplication1/Models/HelloEntity.cs
@@ -11,5 +11,6 @@ namespace WebApplication1.Models
         public string Name { get; set; }
 
         public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
     }
 }
