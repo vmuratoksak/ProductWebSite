0688f549673d84df21e7a19f350f37cfd90c2d28
Veysel Murat OKSAK <vmuratoksak2003@icloud.com>
2026-02-16 13:27:23 +0300

    fena olmadi.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+ï»¿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using Microsoft.Extensions.Options;
 using WebApplication1.Models;
@@ -7,16 +7,19 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IMongoCollection<NameEntity> _collection;
+        private readonly IMongoCollection<NameEntity> _nameCollection;
+        private readonly IMongoCollection<ProductEntity> _productCollection;
+        private readonly IMongoCollection<OrderEntity> _orderCollection;
 
         public HomeController(IMongoClient client, IOptions<MongoSettings> settings)
         {
             var database = client.GetDatabase(settings.Value.DatabaseName);
-            _collection = database.GetCollection<NameEntity>(settings.Value.CollectionName);
 
+            _nameCollection = database.GetCollection<NameEntity>("Names");
+            _productCollection = database.GetCollection<ProductEntity>("Products");
+            _orderCollection = database.GetCollection<OrderEntity>("Orders");
         }
 
-        // Dashboard
         public IActionResult Index()
         {
             var userEmail = HttpContext.Session.GetString("UserEmail");
@@ -24,30 +27,39 @@ namespace WebApplication1.Controllers
             if (string.IsNullOrEmpty(userEmail))
                 return RedirectToAction("Login", "Auth");
 
-            var totalNames = _collection.CountDocuments(_ => true);
+            var totalNames = _nameCollection.CountDocuments(_ => true);
+            var totalProducts = _productCollection.CountDocuments(_ => true);
+            var totalOrders = _orderCollection.CountDocuments(_ => true);
 
             var today = DateTime.Today;
             var tomorrow = today.AddDays(1);
 
-            var todayAdded = _collection.CountDocuments(x =>
+            var todayAdded = _nameCollection.CountDocuments(x =>
                 x.CreatedAt >= today && x.CreatedAt < tomorrow);
 
+            var lastUpdatedName = _nameCollection
+                .Find(_ => true)
+                .SortByDescending(x => x.UpdatedAt)
+                .FirstOrDefault();
+
             ViewBag.TotalNames = totalNames;
+            ViewBag.TotalProducts = totalProducts;
+            ViewBag.TotalOrders = totalOrders;
             ViewBag.TodayAdded = todayAdded;
+            ViewBag.LastUpdatedName = lastUpdatedName?.Name ?? "Yok";
 
             return View();
         }
 
 
-        // Yönetim Paneli
         public IActionResult Privacy()
         {
             return View();
         }
+
         public IActionResult Technologies()
         {
             return View();
         }
-
     }
 }
--- WebApplication1/Models/HelloIndexViewModel.cs
+++ WebApplication1/Models/HelloIndexViewModel.cs
@@ -5,6 +5,8 @@ namespace WebApplication1.Models
     public class HelloIndexViewModel
     {
         public List<NameEntity> Names { get; set; }
+
+        public string LastUpdatedName { get; set; }
         public string Search { get; set; }
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
